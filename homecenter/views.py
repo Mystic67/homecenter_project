03e9cb62c002c64d95fd018c2f9fb6c2fc393d14
Mystic67 <webmaster@passion-geek.com>
2020-04-27 13:25:53 +0200

    Added roller shutter view

--- homecenter/views.py
+++ homecenter/views.py
@@ -47,5 +47,52 @@ def network(request):
         }
         return render(request, 'homecenter/network.html', context)
 
+def roller_shutter(request):
+    roller_shutter_nodes = Instances.objects.filter(index=1).filter(node__product_type__contains="0x030")
+    if zwave.network.is_ready:
+        nw_state = "On"
+    else:
+        nw_state = "Off"
+    setLevel = 0
+    level = 0
+    if request.is_ajax and request.method == 'POST':
+        jsmessages = {}
+        if not zwave.network.is_ready:
+            jsmessages['warning'] = "Le réseau z-wave est à l'arrêt !"
+            data = {
+                'nw_state': nw_state,
+                'messages': jsmessages
+            }
+            return JsonResponse(data)
+        else:
+            node_id = request.POST.get('node_id')
+            setLevel = request.POST.get('setLevel')
+            stop = request.POST.get('stop')
+
+            jsmessages.clear()
+            rollershutter = Rollershutter(zwave.network, int(node_id))
+            if not rollershutter.is_ready:
+                jsmessages['warning'] = "Le modules {} n'est pas prêt.".format(node_id)
+                data = {'messages': jsmessages}
+                return JsonResponse(data)
+            else:
+                if int(stop) == 0:
+                    jsmessages['success'], level = rollershutter.set_open_level(int(setLevel))
+                else:
+                    if int(stop) == 1:
+                        print(rollershutter.level)
+                        print(rollershutter.infos())
+
+                data = {'messages': jsmessages, 'level': level}
+                return JsonResponse(data)
+    else:
+        context = {
+            "setLevel": setLevel,
+            "level": level,
+            "nw_state": nw_state,
+            "roller_shutter_nodes": roller_shutter_nodes
+        }
+        return render(request, 'homecenter/roller_shutter.html', context)
+
 
 
