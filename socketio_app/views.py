5d27df789b00e6df8db204fde33c5d20cc2f118c
Mystic67 <webmaster@passion-geek.com>
2020-06-05 01:56:06 +0200

    Put socketio server to threading async_mode

--- socketio_app/views.py
+++ socketio_app/views.py
@@ -1,17 +1,18 @@
+from django.shortcuts import render
+
 # Create your views here.
 import socketio
 
 # create a Socket.IO server
-sio = socketio.Server(async_mode='eventlet',
+sio = socketio.Server(async_mode='threading',
                       # implémente les origines autorisées
                       cors_allowed_origins='*',
                       # décommente pour activer la journalisation
                       engineio_logger=True,
-                      logger = True,
+                      logger=True,
                       )
 
 
-@sio.event
-def my_event(sid, message):
-    sio.emit('instance states', {'data': message})
-
+@sio.on('connect')
+def connect(sid, environ):
+    print('Le serveur est connecté ', sid)
