6e939646cadcd391cdaffd358e0690c2be23319f
Mystic67 <webmaster@passion-geek.com>
2020-06-05 01:46:32 +0200

    Change roller shutter stop method, some text and reformat PEP8

--- homecenter/backend/rollershutter.py
+++ homecenter/backend/rollershutter.py
@@ -1,18 +1,3 @@
-import openzwave
-import time
-
-from openzwave.command import ZWaveNodeSwitch
-from openzwave.node import ZWaveNode
-from openzwave.value import ZWaveValue
-from openzwave.scene import ZWaveScene
-from openzwave.controller import ZWaveController
-from openzwave.network import ZWaveNetwork
-from openzwave.option import ZWaveOption
-from pydispatch import dispatcher
-from django.conf import settings
-
-
-
 class Rollershutter():
     def __init__(self, network, node_id):
         self.network = network
@@ -51,17 +36,17 @@ class Rollershutter():
 
     @property
     def level(self):
-        #self.nodes[self.node_id].refresh_value(self.get_level_value_id())
-        #self.nodes[self.node_id].request_state()
-        # level = self.nodes[self.node_id].get_dimmer_level(self.value_id)
-        level = [val.data for val in self.nodes[self.node_id].get_values_for_command_class(38).values()][4]
+        level = [val.data for val in
+                 self.nodes[self.node_id].get_values_for_command_class(38)
+                     .values()][4]
         print("level_value_id: {} ".format(self.get_level_value_id()))
         print("level: {} ".format(level))
         return level
 
     @property
     def switch_state(self):
-        state = self.nodes[self.node_id].get_switch_state(self.get_switch_value_id())
+        state = self.nodes[self.node_id].get_switch_state(
+            self.get_switch_value_id())
         return state
 
     @property
@@ -90,43 +75,57 @@ class Rollershutter():
 
     def open(self):
         message = "Le volet s'ouvre !"
-        self.network.nodes[self.node_id].set_switch(self.get_switch_value_id(), True)
-        state = self.switch_state
-        return message, state
+        self.network.nodes[self.node_id].set_switch(self.get_switch_value_id(),
+                                                    True)
+        level = "100"
+        return message, level
 
     def close(self):
-        message = "Le volet se ferme fermeture."
-        self.network.nodes[self.node_id].set_switch(self.get_switch_value_id(), False)
-        state = self.switch_state
-        return message, state
+        message = "Le volet se ferme fermeture !"
+        self.network.nodes[self.node_id].set_switch(self.get_switch_value_id(),
+                                                    False)
+        level = "0"
+        return message, level
 
-    def stop(self):
-        self.network.nodes[self.node_id].set_dimmer(self.value_id, self.level)
-        return self.level
+    def stop(self, value):
+        message = "Le volet a été stoppé !."
+        self.network.nodes[self.node_id].set_switch(self.get_switch_value_id(),
+                                                    value)
+        return message, self.level
 
     def set_open_level(self, set_level):
-        message = "Le volet se met en position à {} % d'ouverture.".format(set_level)
         self.network.nodes[self.node_id].set_dimmer(self.value_id, set_level)
+        if set_level == 99:
+            set_level = 100
+        message = "Le volet se met en position à {} % d'ouverture.".format(
+            set_level)
         return message, self.level
 
     def get_switch_value_id(self):
-        node_value_id = [key for key in self.nodes[self.node_id].get_switches().keys()].pop()
+        node_value_id = [key for key in
+                         self.nodes[self.node_id].get_switches().keys()].pop()
         return node_value_id
 
     @property
     def value_id(self):
-        node_values = [key for key in self.nodes[self.node_id].get_dimmers().keys()].pop()
+        node_values = [key for key in
+                       self.nodes[self.node_id].get_dimmers().keys()].pop()
         return node_values
 
     @property
     def power(self):
-        power = [val.data for val in self.nodes[self.node_id].get_values_for_command_class(50).values()][1]
+        power = [val.data for val in
+                 self.nodes[self.node_id].get_values_for_command_class(
+                     50).values()][1]
         return power
 
     def get_level_value_id(self):
-        level_value_id = [val.value_id for val in self.nodes[self.node_id].get_values_for_command_class(38).values()][4]
+        level_value_id = [val.value_id for val in
+                          self.nodes[self.node_id].get_values_for_command_class(
+                              38).values()][4]
         return level_value_id
 
     def infos(self):
         data = self.nodes[self.node_id].to_dict()
         return data
+
