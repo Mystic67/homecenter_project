a75f00dc0f47bbf9ccafffe876a43c0aeba9d8f8
Mystic67 <webmaster@passion-geek.com>
2020-06-05 01:59:43 +0200

    Remove unused import and reformat PEP8

--- homecenter/backend/zwave.py
+++ homecenter/backend/zwave.py
@@ -1,12 +1,6 @@
-# set async_mode to 'threading', 'eventlet', 'gevent' or 'gevent_uwsgi' to
-# force a mode else, the best mode is selected automatically from what's
-# installed
-async_mode = None
-
 import sys
 import time
 import logging
-
 from django.conf import settings
 from openzwave.object import ZWaveException
 from openzwave.option import ZWaveOption
@@ -30,7 +24,9 @@ class Zwave:
     def options(self):
         # Define some manager options
         try:
-            options = ZWaveOption(settings.DEVICE, config_path=settings.CONFIG_PATH, user_path=settings.USER_PATH,
+            options = ZWaveOption(settings.DEVICE,
+                                  config_path=settings.CONFIG_PATH,
+                                  user_path=settings.USER_PATH,
                                   cmd_line=settings.CMD_LINE)
             options.set_log_file(settings.LOG_FILE)
             options.set_append_log_file(settings.APPEND_LOG_FILE)
@@ -49,15 +45,17 @@ class Zwave:
             print("***** Le réseau est déjà démarré ! *****")
         else:
             start_listener(sio, 'homecenter')
-            #start_listener()
+            # start_listener()
             self.network.start()
 
             # We wait for the network.
-            print("***** Le réseau est en cours de démarrage. Veuillez patienter. *****")
+            print("***** Le réseau est en cours de démarrage. "
+                  "Veuillez patienter. *****")
             for i in range(0, 90):
                 if self.network.state >= self.network.STATE_READY:
                     print("***** Le réseau est prêt ! *****")
-                    print("Le controlleur réseau est : {}".format(self.network.controller))
+                    print("Le controlleur réseau est : {}".
+                          format(self.network.controller))
 
                     self.db.update_database()
                     break
@@ -68,7 +66,8 @@ class Zwave:
 
     def stop(self):
         if self.network.is_ready:
-            print("***** Le réseau est en cours de d'arrêt. Veuillez patienter. *****")
+            print("***** Le réseau est en cours de d'arrêt. "
+                  "Veuillez patienter. *****")
             self.network.stop()
             stop_listener()
         else:
