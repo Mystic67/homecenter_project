f5c84eae66c6951835d95bdb382acb412646ca7e
Mystic67 <webmaster@passion-geek.com>
2020-06-05 17:55:37 +0200

    update management runserver commands to use gevent async-mode in development mode

--- socketio_app/management/commands/runserver.py
+++ socketio_app/management/commands/runserver.py
@@ -11,7 +11,30 @@ class Command(RunCommand):
         if sio.async_mode == 'threading':
             super(Command, self).handle(*args, **options)
         elif sio.async_mode == 'eventlet':
+            # deploy with eventlet
             import eventlet
             import eventlet.wsgi
             from homecenter_project.wsgi import application
-            eventlet.wsgi.server(eventlet.listen(('', 8001)), application)
+            eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
+        elif sio.async_mode == 'gevent':
+            # deploy with gevent
+            from gevent import pywsgi
+            from homecenter_project.wsgi import application
+            try:
+                from geventwebsocket.handler import WebSocketHandler
+                websocket = True
+            except ImportError:
+                websocket = False
+            if websocket:
+                pywsgi.WSGIServer(
+                    ('', 8000), application,
+                    handler_class=WebSocketHandler).serve_forever()
+            else:
+                pywsgi.WSGIServer(('', 8000), application).serve_forever()
+        elif sio.async_mode == 'gevent_uwsgi':
+            print('Start the application through the uwsgi server. Example:')
+            print('uwsgi --http :5000 --gevent 1000 --http-websockets '
+                  '--master --wsgi-file django_example/wsgi.py --callable '
+                  'application')
+        else:
+            print('Unknown async_mode: ' + sio.async_mode)
